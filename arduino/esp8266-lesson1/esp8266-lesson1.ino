ed812a7328d77de09744f02e4b86e9759db302b3
Nuno Mendes <nuno.mendes@picowireless.net>
2022-10-04 22:49:06 +0100

    20221004 2248
    primeiro teste do arduino com wifi

---
+++ arduino/esp8266-lesson1/esp8266-lesson1.ino
@@ -0,0 +1,156 @@
+/*  ___   ___  ___  _   _  ___   ___   ____ ___  ____  
+ * / _ \ /___)/ _ \| | | |/ _ \ / _ \ / ___) _ \|    \ 
+ *| |_| |___ | |_| | |_| | |_| | |_| ( (__| |_| | | | |
+ * \___/(___/ \___/ \__  |\___/ \___(_)____)___/|_|_|_|
+ *                  (____/ 
+ * Osoyoo Wifi Arduino Shield Testing Project
+ * Make Arduino and ESP8266 wifi shield as a web browser
+ * tutorial url: https://osoyoo.com/?p=32510
+ */
+
+/*  ___   ___  ___  _   _  ___   ___   ____ ___  ____  
+ * / _ \ /___)/ _ \| | | |/ _ \ / _ \ / ___) _ \|    \ 
+ *| |_| |___ | |_| | |_| | |_| | |_| ( (__| |_| | | | |
+ * \___/(___/ \___/ \__  |\___/ \___(_)____)___/|_|_|_|
+ *                  (____/ 
+ * Use browser and OSOYOO MEGA-IoT extension shield to control LED from remote browser
+ * Tutorial URL http://osoyoo.com/?p=28284
+ * CopyRight www.osoyoo.com
+ */
+#include "WiFiEsp.h"
+
+ 
+//#ifndef HAVE_HWSERIAL1
+#include "SoftwareSerial.h"
+SoftwareSerial softserial(4, 5); // RX, TX
+//#endif
+
+char ssid[] = "smartenergy";            // your network SSID (name)
+char pass[] = "20222023lesi";        // your network password
+int status = WL_IDLE_STATUS;
+int reqCount = 0;                // number of requests received
+
+WiFiEspServer server(80);
+
+
+
+void setup()
+{
+
+  // initialize serial for debugging
+  Serial.begin(9600);
+  // initialize serial for ESP module
+    softserial.begin(115200);
+  softserial.write("AT+CIOBAUD=9600\r\n");
+  softserial.write("AT+RST\r\n");
+  softserial.begin(9600);
+  // initialize ESP module
+  WiFi.init(&softserial);
+
+  // check for the presence of the shield
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue
+    while (true);
+  }
+
+  // attempt to connect to WiFi network
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network
+    status = WiFi.begin(ssid, pass);
+  }
+
+  Serial.println("You're connected to the network");
+  printWifiStatus();
+  
+  // start the web server on port 80
+  server.begin();
+}
+
+
+void loop()
+{
+/*
+  Serial.println("Project: Smart Energy Campus");
+  Serial.println("Year: 2022 / 2023");
+  Serial.println("      LESI @ IPCA");
+  Serial.println("Authors: 2727 Nuno Mendes");
+  Serial.println("         Tiago Azevedo");
+  Serial.println("         Francisco Pereira");
+  Serial.println("         Rosario Silva");
+  */
+  // listen for incoming clients
+  WiFiEspClient client = server.available();
+  if (client) {
+    Serial.println("New client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          Serial.println("Sending response");
+          
+          // send a standard http response header
+          // use \r\n instead of many println statements to speedup data send
+          client.print(
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/html\r\n"
+            "Connection: close\r\n"  // the connection will be closed after completion of the response
+            "Refresh: 20\r\n"        // refresh the page automatically every 20 sec
+            "\r\n");
+          client.print("<!DOCTYPE HTML>\r\n");
+          client.print("<html>\r\n");
+          client.print("<h4>ESP8266 Wifi IoT lesson 1</h4>\r\n");
+          client.print("<h1>Hello World!</h1>\r\n");
+          client.print("Requests received: ");
+          client.print(++reqCount);
+          client.print("<br>\r\n");
+   
+    
+          client.print("</html>\r\n");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        }
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(10);
+
+    // close the connection:
+    client.stop();
+    Serial.println("Client disconnected");
+  }
+}
+
+
+void printWifiStatus()
+{
+  // print the SSID of the network you're attached to
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  
+  // print where to go in the browser
+  Serial.println();
+  Serial.print("To see this page in action, open a browser to http://");
+  Serial.println(ip);
+  Serial.println();
+}
---
+++ arduino/v1/v1.ino
@@ -0,0 +1,71 @@
+/*
+from: https://tttapa.github.io/
+      https://tttapa.github.io/ESP8266/Chap01%20-%20ESP8266.html
+
+Open the Arduino IDE.
+Go to File > Preferences.
+Paste the URL http://arduino.esp8266.com/stable/package_esp8266com_index.json into the Additional Board Manager URLs field.
+(You can add multiple URLs, separating them with commas.)
+Go to Tools > Board > Board Manager and search for 'esp8266'. Select the newest version, and click install. (As of February 7th 2017, the latest stable version is 2.3.0.)
+You can check out the official install guide: https://github.com/esp8266/Arduino/blob/master/doc/installing.md
+*/
+
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WiFiMulti.h> 
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>   // Include the WebServer library
+
+ESP8266WiFiMulti wifiMulti;     // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
+
+ESP8266WebServer server(80);    // Create a webserver object that listens for HTTP request on port 80
+
+void handleRoot();              // function prototypes for HTTP handlers
+void handleNotFound();
+
+void setup(void){
+  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
+  delay(10);
+  Serial.println('\n');
+
+  wifiMulti.addAP("smartenergy", "20222023lesi");   // add Wi-Fi networks you want to connect to
+  //wifiMulti.addAP("ssid_from_AP_2", "your_password_for_AP_2");
+  //wifiMulti.addAP("ssid_from_AP_3", "your_password_for_AP_3");
+
+  Serial.println("Connecting ...");
+  int i = 0;
+  while (wifiMulti.run() != WL_CONNECTED) { // Wait for the Wi-Fi to connect: scan for Wi-Fi networks, and connect to the strongest of the networks above
+    delay(250);
+    Serial.print('.');
+  }
+  Serial.println('\n');
+  Serial.print("Connected to ");
+  Serial.println(WiFi.SSID());              // Tell us what network we're connected to
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());           // Send the IP address of the ESP8266 to the computer
+
+  if (MDNS.begin("esp8266")) {              // Start the mDNS responder for esp8266.local
+    Serial.println("mDNS responder started");
+  } else {
+    Serial.println("Error setting up MDNS responder!");
+  }
+
+  server.on("/", handleRoot);               // Call the 'handleRoot' function when a client requests URI "/"
+  server.onNotFound(handleNotFound);        // When a client requests an unknown URI (i.e. something other than "/"), call function "handleNotFound"
+
+  server.begin();                           // Actually start the server
+  Serial.println("HTTP server started");
+}
+
+void loop(void){
+  server.handleClient();                    // Listen for HTTP requests from clients
+}
+
+void handleRoot() {
+  server.send(200, "text/plain", "Hello world!");   // Send HTTP status 200 (Ok) and send some text to the browser/client
+}
+
+void handleNotFound(){
+  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
+}
