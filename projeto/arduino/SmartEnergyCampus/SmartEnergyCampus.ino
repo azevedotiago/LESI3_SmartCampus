1ceb987657e9e2955cdb8149fbb0d5ff2ce34f5d
Nuno Mendes <nuno.mendes@picowireless.net>
2022-11-07 19:20:38 +0000

    Update SmartEnergyCampus.ino

--- projeto/arduino/SmartEnergyCampus/SmartEnergyCampus.ino
+++ projeto/arduino/SmartEnergyCampus/SmartEnergyCampus.ino
@@ -1,12 +1,12 @@
 /*
   
-  Project: Smart Energy Campus");
-  Year: 2022 / 2023");
-        LESI @ IPCA");
-  Authors: 2727 Nuno Mendes");
-           21138 Rosario Silva");
-           21153 Tiago Azevedo");
-           21156 Francisco Pereira");
+  Project: Smart Energy Campus
+  Year: 2022 / 2023
+        LESI @ IPCA
+  Authors: 2727 Nuno Mendes
+           21138 Rosario Silva
+           21153 Tiago Azevedo
+           21156 Francisco Pereira
 
 */
 
