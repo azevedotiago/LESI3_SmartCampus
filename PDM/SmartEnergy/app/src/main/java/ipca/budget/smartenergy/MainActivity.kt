66354da11306a009ad1b720f34dfa42ebb452d4c
Nuno Mendes <nuno.mendes@picowireless.net>
2022-12-15 20:13:16 +0000

    login.php

--- PDM/SmartEnergy/app/src/main/java/ipca/budget/smartenergy/MainActivity.kt
+++ PDM/SmartEnergy/app/src/main/java/ipca/budget/smartenergy/MainActivity.kt
@@ -1,11 +1,49 @@
 package ipca.budget.smartenergy
+// https://github.com/jayfirke/Login-With-Kotlin-and-Php-MySQL
+// https://androidjson.com/android-server-login-registration-php-mysql/
 
-import android.support.v7.app.AppCompatActivity
+// https://www.tutorialkart.com/kotlin-android/login-form-example-in-kotlin-android/
+
+// remover o titulo da app na janela de login
+// https://stackoverflow.com/questions/36236181/how-to-remove-title-bar-from-the-android-activity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        try {
+            this.supportActionBar!!.hide()
+        } catch (e: NullPointerException) {
+        }
+
         setContentView(R.layout.activity_main)
+
+        // get reference to all views
+        var et_user_name = findViewById(R.id.et_user_name) as EditText
+        var et_password = findViewById(R.id.et_password) as EditText
+        var btn_reset = findViewById(R.id.btn_reset) as Button
+        var btn_submit = findViewById(R.id.btn_submit) as Button
+
+        btn_reset.setOnClickListener {
+            // clearing user_name and password edit text views on reset button click
+            et_user_name.setText("")
+            et_password.setText("")
+        }
+
+        // set on-click listener
+        btn_submit.setOnClickListener {
+            val user_name = et_user_name.text;
+            val password = et_password.text;
+            Toast.makeText(this@MainActivity, user_name, Toast.LENGTH_LONG).show()
+
+            // your code to validate the user_name and password combination
+            // and verify the same
+
+        }
     }
 }
---
+++ projeto/servidor/www/login.php
@@ -0,0 +1,48 @@
+<?php  
+date_default_timezone_set("Europe/Lisbon");
+include 'config.php';
+$output = array();
+$input = array();
+$response = array();
+
+if($_GET['method']=="login"){
+		$username= $_GET["username"];
+	    $password= $_GET["password"];
+
+		// ligacao a base de dados
+		$mysqli = new mysqli($bd_server,  $bd_user, $bd_passwd, $bd);
+	
+		// verifica a conetividade com a base de dados
+		if ($mysqli === false) {
+			die("ERROR: Could not connect. ".$mysqli->connect_error);
+		} else {
+			echo "<br>sql connection sucessfull<br>";
+		}
+		
+		// query a base de dados se existe o username com a password
+    	$sql = "select * from users where username='$username' and password='$password'";
+		$result = $mysqli->query($sql);
+		
+		if($result->num_rows > 0){
+			// inicializa valores de nao acesso / login invalido
+			$output['response'] = "false";
+			$output['userid'] = "no_record_found";
+			$output['name'] = "no_record_found";
+			$output['username'] = "no_record_found";
+
+			// coloca os valores corretos do login, username, name
+			$row = $result->fetch_assoc();
+			$output["response"] = trim("true");
+			$output["userid"] = trim($row["idusers"]);
+			$name = str_replace(" ","_",$row['name']);
+			$output["name"] = trim($name);
+			$username = str_replace(" ","_",$row['username']);
+			$output["username"] = trim($username);
+		}else{
+			$output['response'] = "false";
+			$output['userid'] = "no_record_found";
+			$output['name'] = "no_record_found";
+			$output['username'] = "no_record_found";
+		}
+	echo json_encode($output);
+	}
