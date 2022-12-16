4f2471edcb16c56bdc826ee5ed338df77c3af813
rosário <a21138@alunos.ipca.pt>
2022-12-16 16:37:38 +0000

    Update dataset.py

---
+++ IA/TP2/IA_Projeto02_G4/dataset.py
@@ -0,0 +1,14 @@
+import copy
+from collections import defaultdict
+from statistics import stdev
+
+from qpsolvers import solve_qp
+
+from deep_learning4e import Sigmoid
+from probabilistic_learning import NaiveBayesLearner
+from utils4e import *
+
+class DataSet:
+    # Inicialização 
+    def _init_():
+        
