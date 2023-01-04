0f17c370fb778bac5d9c3e1f54da85c07b6e0020
Nuno Mendes <nuno.mendes@picowireless.net>
2023-01-04 14:25:26 +0000

    Update config.php

--- projeto/servidor/www/config.php
+++ projeto/servidor/www/config.php
@@ -22,6 +22,6 @@ $bd_user = "se";                 // Username
 $bd_passwd = "smartenergy";      // Palavra Passe
 
 // tempos
-$onlineTime = "90:05:00";		// tempo para considerar um poste de iluminacao online (HH:MM:SS)
+$onlineTime = "00:03:00";		// tempo para considerar um poste de iluminacao online (HH:MM:SS)
 ?>
 
