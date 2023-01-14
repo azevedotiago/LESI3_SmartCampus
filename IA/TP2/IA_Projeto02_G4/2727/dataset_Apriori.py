7f82ccd482e1a14bca2bd4fece6209a4a343a7b3
Nuno Mendes <nuno.mendes@picowireless.net>
2023-01-14 17:53:17 +0000

    Update dataset_Apriori.py

--- IA/TP2/IA_Projeto02_G4/2727/dataset_Apriori.py
+++ IA/TP2/IA_Projeto02_G4/2727/dataset_Apriori.py
@@ -4,27 +4,28 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from apyori import apriori
 
-Data = pd.read_csv('BankChurners.csv')
+Data = pd.read_csv('BankChurners.csv') #, header = None)
 
 # Intializing the list
 transacts = []
-
 # populating a list of transactions
-for i in range(0, 100): 
+for i in range(0, 10127): 
   transacts.append([str(Data.values[i,j]) for j in range(0, 20)])
 
 rule = apriori(transactions = transacts, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2, max_length = 2)
 
 output = list(rule) # returns a non-tabular output
 # putting output into a pandas dataframe
-results = 0
+
 def inspect(output):
-    lhs         = [tuple(result[2][0][0])[0] for result in output]
-    rhs         = [tuple(result[2][0][1])[0] for result in output]
+    lhs        = [tuple(result[2][0][0])[0] for result in output]
+    rhs        = [tuple(result[2][0][1])[0] for result in output]
     support    = [result[1] for result in output]
     confidence = [result[2][0][2] for result in output]
     lift       = [result[2][0][3] for result in output]
     return list(zip(lhs, rhs, support, confidence, lift))
-output_DataFrame = pd.DataFrame(inspect(results), columns = ['Customer_Age', 'Attrition_Flag','Dependent_count', 'Months_on_book', 'Total_Relationship_Count', 'Avg_Open_To_Buy'])
+output_DataFrame = pd.DataFrame(inspect(output), columns = ['Left_Hand_Side', 'Right_Hand_Side', 'Support', 'Confidence', 'Lift'])
 
-output_DataFrame.nlargest(n = 10, columns = 'Avg_Open_To_Buy')
+output_DataFrame.nlargest(n = 10, columns = 'Lift')
+
+print(output_DataFrame)
